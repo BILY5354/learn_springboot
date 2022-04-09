028ba26e49d192aed1d8e6b5060b0a67645a667e
BILY5354 <2587547190@qq.com>
2022-04-10 00:03:47 +0800

    complete p53 -> finish the base stage

--- springboot_08_ssmp/src/main/java/com/itheima/controller/BookController.java
+++ springboot_08_ssmp/src/main/java/com/itheima/controller/BookController.java
@@ -51,12 +51,22 @@ public class BookController {
         return new R(true, bookService.getById(id));
     }
 
-    @GetMapping("{currentPage}/{pageSize}")
+    /*@GetMapping("{currentPage}/{pageSize}")
     public R getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
         IPage<Book> page = bookService.getPage(currentPage, pageSize);
         //如果当前页码值大于总页码值，那么重新执行查询操作，使用最大页码值作为当前页码值
         if (currentPage > page.getPages()) {
             page = bookService.getPage((int) page.getPages(), pageSize);
+        }
+        return new R(true, page);
+    }*/
+
+    @GetMapping("{currentPage}/{pageSize}")
+    public R getPage(@PathVariable int currentPage, @PathVariable int pageSize, Book book) {
+        IPage<Book> page = bookService.getPage(currentPage, pageSize, book);
+        //如果当前页码值大于总页码值，那么重新执行查询操作，使用最大页码值作为当前页码值
+        if (currentPage > page.getPages()) {
+            page = bookService.getPage((int) page.getPages(), pageSize, book);
         }
         return new R(true, page);
     }
--- springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
+++ springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
@@ -11,4 +11,6 @@ import com.itheima.domain.Book;
 public interface IBookService extends IService<Book> {
 
     IPage<Book> getPage(int currentPage, int pageSize);
+
+    IPage<Book> getPage(int currentPage, int pageSize, Book book);
 }
