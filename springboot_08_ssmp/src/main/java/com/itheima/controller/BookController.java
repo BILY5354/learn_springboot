dfcd4c86a1da167149d776cccc1c1e5f2ad86b75
BILY5354 <2587547190@qq.com>
2022-04-09 10:16:57 +0800

    completed p42

--- springboot_08_ssmp/src/main/java/com/itheima/controller/BookController.java
+++ springboot_08_ssmp/src/main/java/com/itheima/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.itheima.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.itheima.controller.utils.R;
 import com.itheima.domain.Book;
 import com.itheima.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,32 +21,37 @@ public class BookController {
     private IBookService bookService;
 
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.list();
+    public R getAll() {
+
+        return new R(true, bookService.list());
     }
 
     @PostMapping
-    public Boolean save(@RequestBody Book book) {
-        return bookService.save(book);
+    public R save(@RequestBody Book book) {
+
+        return new R(bookService.save(book));
     }
 
     @PutMapping
-    public Boolean update(@RequestBody Book book) {
-        return bookService.updateById(book);
+    public R update(@RequestBody Book book) {
+
+        return new R(bookService.updateById(book));
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Integer id) {
-        return bookService.removeById(id);
+    public R delete(@PathVariable Integer id) {
+
+        return new R(bookService.removeById(id));
     }
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Integer id) {
-        return bookService.getById(id);
+    public R getById(@PathVariable Integer id) {
+
+        return new R(true, bookService.getById(id));
     }
 
     @GetMapping("{currentPage}/{pageSize}")
-    public IPage<Book> getPage(@PathVariable int currentPage,@PathVariable int pageSize) {
-        return bookService.getPage(currentPage,pageSize);
+    public R getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
+        return new R(true, bookService.getPage(currentPage, pageSize));
     }
 }
---
+++ springboot_08_ssmp/src/main/java/com/itheima/controller/utils/R.java
@@ -0,0 +1,27 @@
+package com.itheima.controller.utils;
+
+import lombok.Data;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+/**
+ * @author CDUY
+ * @version 1.0
+ */
+@Data
+public class R {
+    private Boolean flag;
+    private Object data;
+
+    public R() {
+
+    }
+
+    public R(Boolean flag) {
+        this.flag = flag;
+    }
+
+    public R(Boolean flag, Object data) {
+        this.flag = flag;
+        this.data = data;
+    }
+}
