7e6193eddfe0f1d5c4c3759da5a13ee133b1af0e
BILY5354 <2587547190@qq.com>
2022-04-09 09:59:54 +0800

    completed p41

--- springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
+++ springboot_08_ssmp/src/main/java/com/itheima/service/IBookService.java
@@ -1,5 +1,6 @@
 package com.itheima.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.domain.Book;
 
@@ -8,4 +9,6 @@ import com.itheima.domain.Book;
  * @version 1.0
  */
 public interface IBookService extends IService<Book> {
+
+    IPage<Book> getPage(int currentPage, int pageSize);
 }
